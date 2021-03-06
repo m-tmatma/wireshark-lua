8ccaf3dff0111dccf83332667ad737560439470f
Masaru Tsuchiyama <m.tmatma@gmail.com>
2021-03-06 09:11:45 +0900

    add subtree

--- wsdiscovery.lua
+++ wsdiscovery.lua
@@ -4,8 +4,9 @@ function WSDiscovery_proto.dissector(buffer,pinfo,tree)
     pinfo.cols.protocol = "WSDiscovery"
     pinfo.cols.info = "WSDiscovery"
 
+    local subTree = tree:add (WSDiscovery_proto, buffer())
     xml_dissector = Dissector.get("xml")
-    xml_dissector:call(buffer,pinfo,tree)
+    xml_dissector:call(buffer,pinfo,subTree)
 end
 
 DissectorTable.get("udp.port"):add(3702, WSDiscovery_proto);

b8efb27fdb9624e727fde2aab419c0d4047fe36d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-09-17 09:24:44 +0900

    don't include subtree.

--- wsdiscovery.lua
+++ wsdiscovery.lua
@@ -3,10 +3,8 @@ WSDiscovery_proto = Proto("WSDiscovery","WSDiscovery","WSDiscovery protocol")
 function WSDiscovery_proto.dissector(buffer,pinfo,tree)
     pinfo.cols.protocol = "WSDiscovery"
     pinfo.cols.info = "WSDiscovery"
-
-    local subTree = tree:add (WSDiscovery_proto, buffer())
     xml_dissector = Dissector.get("xml")
-    xml_dissector:call(buffer,pinfo,subTree)
+    xml_dissector:call(buffer,pinfo,tree)
 end
 
 DissectorTable.get("udp.port"):add(3702, WSDiscovery_proto);
